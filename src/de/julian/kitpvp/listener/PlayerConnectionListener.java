b000e264ee9310b6e3e878eb4620036bb73d7202
Julian Justin <justinofju@gmail.com>
2019-10-04 21:01:58 +0200

    First Commit

---
+++ src/de/julian/kitpvp/listener/PlayerConnectionListener.java
@@ -0,0 +1,14 @@
+package de.julian.kitpvp.listener;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+
+public class PlayerConnectionListener implements Listener {
+
+    @EventHandler
+    public void handleJoin(PlayerJoinEvent e) {
+
+    }
+
+}
---
+++ src/de/julian/kitpvp/main/Main.java
@@ -0,0 +1,11 @@
+package de.julian.kitpvp.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Now it begins...");
+    }
+}
